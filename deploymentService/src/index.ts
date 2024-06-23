ca704384637a67dc15b8df07801158a635861166
cosmos <91248142+cosmos1721@users.noreply.github.com>
2024-06-24 04:28:57 +0530

    final commit

--- deploymentService/src/index.ts
+++ deploymentService/src/index.ts
@@ -2,9 +2,47 @@ import {createClient, commandOptions} from "redis";
 import { downloadBuildFolder, uploadBuildFiles } from "./aws";
 import { build, fetchAllFiles } from "./utils";
 import path from "path";
+import dotenv from "dotenv";
+dotenv.config();
 
-const subscriber = createClient();
+const redisUrl = process.env.REDIS_URL;
+if (!redisUrl) {
+  throw new Error("REDIS_URL is required");
+}
+
+const subscriber = createClient({
+    url: redisUrl
+});
+
 subscriber.connect();
+
+async function processBuild(buildId: string) {
+try {
+    await downloadBuildFolder(`output/${buildId}`);
+    subscriber.hSet("status", buildId, "Build in progress");
+    await build(buildId);
+    
+    const dirPath = path.join(__dirname, `output/${buildId}/build`);
+    const files = fetchAllFiles(dirPath);
+    const fileCount = files.length;
+    console.log(`Found ${fileCount} files`);
+    let count = 1;
+    const uploadBuildPromises = files.map(async file => {
+        await uploadBuildFiles(file.slice(__dirname.length + 1), file);
+        process.stdout.write(`\rUpload file count: ${count}/${fileCount}`);
+        count++;
+        });
+
+    await Promise.all(uploadBuildPromises);
+    console.log("\nAll files uploaded");
+
+    subscriber.hSet("status", buildId, "Done");
+
+    }catch (error) {
+        console.error("Error processing build", error);
+        subscriber.hSet("status", buildId, "Failed");
+    }
+}
 
 async function main() {
     console.log("downloading files");
@@ -13,31 +51,15 @@ async function main() {
             commandOptions({ isolated: true }),
             "upload-queue",
             0
-        );
-
-        //@ts-ignore
+            );
+            
+        // @ts-ignore
         const buildId = resp.element;
-        await downloadBuildFolder(`output/${buildId}`);
-        await build(buildId);
-
-        const dirPath = path.join(__dirname, `output/${buildId}/build`);
-        const files = fetchAllFiles(dirPath);
-        const fileCount = files.length;
-        console.log(`Found ${fileCount} files`);
-        let count = 1;
-        const uploadBuildPromises = files.map(async file => {
-            await uploadBuildFiles(file.slice(__dirname.length + 1), file);
-            process.stdout.write(`\rUpload file count: ${count}/${fileCount}`);
-            count++;
-          });
-        await Promise.all(uploadBuildPromises);
-        console.log("\nAll files uploaded");
-
-        subscriber.lPush("build-queue", buildId);
-
+        processBuild(buildId);
     }
 }
-main();
+
+main().catch(err => console.error('Main function error:', err));
 
 
 
